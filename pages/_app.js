f9c97ef0d80db5421ddcc3eb1b30ab81c66b9f8d
linHeMa <linhema0921@gmail.com>
2022-10-26 16:25:44 +0800

    add tailwind css and babelrc

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  console.log('yes')
-  return <Component {...pageProps} />
+  console.log('yes');
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
